ba369ac82540ff75c075bc75377fd48c52073bb2
althafvly <althafvly@gmail.com>
2018-02-25 12:09:40 +0000

    init: compilation errors fix

--- init/init_msm8916.cpp
+++ init/init_msm8916.cpp
@@ -32,14 +32,13 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-#include <android-base/strings.h>
 #include <android-base/logging.h>
 #include <android-base/properties.h>
 #include "property_service.h"
 #include "vendor_init.h"
+
 #include "init_msm8916.h"
 
-using android::base::Trim;
 using android::base::GetProperty;
 using android::init::property_set;
 
@@ -48,16 +47,36 @@ void init_target_properties()
 {
 }
 
+static int read_file2(const char *fname, char *data, int max_size)
+{
+    int fd, rc;
+
+    if (max_size < 1)
+        return 0;
+
+    fd = open(fname, O_RDONLY);
+    if (fd < 0) {
+        LOG(ERROR) <<  "failed to open '" << fname << "'\n";
+        return 0;
+    }
+
+    rc = read(fd, data, max_size - 1);
+    if ((rc > 0) && (rc < max_size))
+        data[rc] = '\0';
+    else
+        data[0] = '\0';
+    close(fd);
+
+    return 1;
+}
+
 static void init_alarm_boot_properties()
 {
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
-    std::string boot_reason;
-    std::string power_off_alarm;
+    char const *alarm_file = "/proc/sys/kernel/boot_reason";
+    char buf[64];
     std::string tmp = GetProperty("ro.boot.alarmboot","");
 
-    if (read_file(boot_reason_file, &boot_reason)
-            && read_file(power_off_alarm_file, &power_off_alarm)) {
+    if (read_file2(alarm_file, buf, sizeof(buf))) {
         /*
          * Setup ro.alarm_boot value to true when it is RTC triggered boot up
          * For existing PMIC chips, the following mapping applies
@@ -73,11 +92,10 @@ static void init_alarm_boot_properties()
          * 7 -> CBLPWR_N pin toggled (for external power supply)
          * 8 -> KPDPWR_N pin toggled (power key pressed)
          */
-        if ((Trim(boot_reason) == "3" || tmp == "true")
-                && Trim(power_off_alarm) == "1")
-            GetProperty("ro.alarm_boot","", "true");
+        if (buf[0] == '3' || tmp == "true")
+            property_set("ro.alarm_boot", "true");
         else
-            GetProperty("ro.alarm_boot","", "false");
+            property_set("ro.alarm_boot", "false");
     }
 }
 
